b293e1a70f228649c8ab8b6e371303f634717b59
Vishalkaman <vishalkaman03@gmail.com>
2025-04-06 13:24:41 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express on Vercel!");
+});
+
+module.exports = app;
+
